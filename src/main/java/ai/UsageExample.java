65d313851d5b41e098655053dcdd3fabe56b7883
pascschm <pascal99.schmidt@googlemail.com>
2019-11-16 21:27:58 +0100

    tests

--- src/main/java/ai/UsageExample.java
+++ src/main/java/ai/UsageExample.java
@@ -6,6 +6,7 @@ import ai.utility.Requirements;
 import api.MSAgent;
 import api.MSField;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -20,13 +21,20 @@ public class UsageExample {
 	 * Array containing the names of all fields. If you want to iterate over all of
 	 * them, this might help.
 	 */
-	public static final String[] fields = { "baby1-3x3-0.txt", "baby2-3x3-1.txt", "baby3-5x5-1.txt", "baby4-5x5-3.txt",
+	public static String[] fields = { "baby1-3x3-0.txt", "baby2-3x3-1.txt", "baby3-5x5-1.txt", "baby4-5x5-3.txt",
 			"baby5-5x5-5.txt", "baby6-7x7-1.txt", "baby7-7x7-3.txt", "baby8-7x7-5.txt", "baby9-7x7-10.txt",
 			"anfaenger1-9x9-10.txt", "anfaenger2-9x9-10.txt", "anfaenger3-9x9-10.txt", "anfaenger4-9x9-10.txt",
 			"anfaenger5-9x9-10.txt", "fortgeschrittene1-16x16-40.txt", "fortgeschrittene2-16x16-40.txt",
 			"fortgeschrittene3-16x16-40.txt", "fortgeschrittene4-16x16-40.txt", "fortgeschrittene5-16x16-40.txt",
 			"profi1-30x16-99.txt", "profi2-30x16-99.txt", "profi3-30x16-99.txt", "profi4-30x16-99.txt",
 			"profi5-30x16-99.txt" };
+
+	/**
+	 * ######################
+	 * Used to test a sample list of fields (null/empty = ignored)
+	 * ######################
+	 */
+	public static String[] testFields = {};
 
 	public static void main(String[] args) {
 
@@ -36,9 +44,13 @@ public class UsageExample {
 		agent.deactivateDisplay();
 		*/
 
-		int iterations = 100;
+		if (testFields != null && testFields.length != 0) {
+			fields = testFields;
+		}
+
 		Requirements req = new Requirements();
 		for (int i = 0; i < fields.length; i++) {
+			int iterations = fields[i].contains("profi") ? 100 : 1000;
 			int solved = 0;
 			long average = 0;
 
